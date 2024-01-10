c35cb37195896239ea00e6f9e3faa479a722b778
Shivang <st.shivanggupta2@gmail.com>
2024-01-10 22:08:26 +0530

    Enabling this service as Eureka-Client and registering it in Eureka-server

--- src/main/java/com/micro/booking/service/FeignClient/PassengerClient.java
+++ src/main/java/com/micro/booking/service/FeignClient/PassengerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:5051", value = "PassengerClient")
+@FeignClient(name = "PASSENGER-SERVICE")
 public interface PassengerClient {
 
     @GetMapping("/passenger/details/{id}")               //this mapping url should be same as in booking service which is already there
